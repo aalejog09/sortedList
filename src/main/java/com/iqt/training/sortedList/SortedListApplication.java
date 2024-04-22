d05f226e2b1cf802eebdfea8dd9c48f116a996ea
Andres Alejo <aalejog09@gmail.com>
2024-04-22 09:41:03 -0400

    initial Commit. base proyect

---
+++ src/main/java/com/iqt/training/sortedList/SortedListApplication.java
@@ -0,0 +1,13 @@
+package com.iqt.training.sortedList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortedListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortedListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iqt/training/sortedList/SortedListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqt.training.sortedList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortedListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
